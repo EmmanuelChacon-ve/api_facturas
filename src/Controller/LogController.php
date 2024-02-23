40678c8d0679459e8a98c0876818d78b6189d549
Emmanuel Chacon <emmanuelechra@gmail.com>
2024-02-23 13:22:04 -0400

    arreglos eventos

--- src/Controller/LogController.php
+++ src/Controller/LogController.php
@@ -1,10 +1,13 @@
 <?php
+
 namespace App\Controller;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use ApiPlatform\Core\Annotation\ApiResource;
 use Doctrine\ORM\EntityManagerInterface;
 use App\Entity\Log;
+use DateTimeImmutable;
 
 class LogController extends AbstractController
 {
@@ -20,6 +23,7 @@ class LogController extends AbstractController
         $method = $request->getMethod();
         $url = $request->getUri();
         $params = $request->query->all();
+        $createdAt = new DateTimeImmutable(); // Fecha y hora actual
 
         $log = new Log();
         $log->setLevel('info');
@@ -27,11 +31,11 @@ class LogController extends AbstractController
         $log->setRequestMethod($method);
         $log->setRequestUrl($url);
         $log->setRequestParams(json_encode($params));
+        $log->setCreatedAt($createdAt); // Establecer la fecha y hora actual
 
         $this->entityManager->persist($log);
         $this->entityManager->flush();
 
-        return $log; // Devuelve el objeto Log, que será serializado automáticamente por API Platform
+        return $log; 
     }
 }
-
